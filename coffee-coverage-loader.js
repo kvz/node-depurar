189a6697b6b2418d762d4beab94e9566ea2a2c16
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-06-21 12:04:47 +0200

    Fix projectRoot

--- coffee-coverage-loader.js
+++ coffee-coverage-loader.js
@@ -3,7 +3,8 @@
 
 var path = require('path');
 var coffeeCoverage = require('coffee-coverage');
-var projectRoot = path.resolve(__dirname, "../..");
+var projectRoot = path.resolve(__dirname);
+// console.log(projectRoot);
 var coverageVar = coffeeCoverage.findIstanbulVariable();
 // Only write a coverage report if we're not running inside of Istanbul.
 var writeOnExit = (coverageVar == null) ? (projectRoot + '/coverage/coverage-coffee.json') : null;
